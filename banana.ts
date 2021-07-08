64868e61b3bc7fbd84507603a6815e5233967793
Brad Pickering <brad.pickering@dice.se>
2021-07-08 15:50:48 -0700

    changes from tropic example extension

--- banana.ts
+++ banana.ts
@@ -1,26 +1,92 @@
 /**
- * Types of tropical fruit
+ * Appearance and condition of the banana.
  */
- enum TropicalFruit {
-    Banana = 0,
-    Pinapple = 1,
-    Coconut = 2
+enum Peel {
+    //% block=green
+    Green = 0,
+    //% block=yellow
+    Yellow = 1,
+    //% block=spotted
+    Spotted = 2,
+    //% block=blackened
+    Blackened = 3,
+    //% block=peeled
+    Peeled = 4
 }
 
-/**
- * Pick some fruit and peel it.
- */
 namespace tropic {
     /**
-     * Pick a fruit
+     * The tropical fruit called banana.
      */
-    export function pick(fruit: TropicalFruit): boolean {
-        return true;
+    export class banana {
+        _condition: Peel;
+
+        constructor() {
+            this._condition = Peel.Green;
+        }
+        /**
+         * Make the banana get riper.
+         * @param tooMuch make the banana ripen too much, eg: false
+         */
+        //% weight=66
+        //% blockId=banana_ripen block="%fruit|ripen"
+        public ripen(tooMuch: boolean = false): void {
+            if (tooMuch) {
+                this._condition = Peel.Blackened;
+            } else if (this._condition <= Peel.Spotted) {
+                this._condition = this._condition + 1;
+            }
+        }
+        /**
+         * Peel the skin off of the banana
+         */
+        //% weight=65
+        //% blockId=banana_peel block="%fruit|peel"
+        public peel(): void {
+            this._condition = Peel.Peeled;
+        }
+        /**
+         * See if the banana is ripe yet.
+         */
+        //% weight=64
+        //% blockId=banana_ripe block="%fruit|ripe"
+        public ripe(): boolean {
+            return (this._condition > Peel.Green);
+        }
+        /**
+         * See how ripe the banana is now.
+         */
+        //% weight=63
+        //% blockId=banana_how_ripe block="%fruit|how ripe"
+        public howRipe(): Peel {
+            return this._condition;
+        }
+        /**
+         * Let the banana go rotten.
+         */
+        //% weight=62
+        //% blockId=banana_rot block="%fruit|let rot"
+        public rot(): void {
+            this._condition = Peel.Blackened;
+        }
     }
+
     /**
-     * Peel the fruit if possible
+     * Pick a fruit to ripen. Just bananas right now.
      */
-    export function peel(fruit: TropicalFruit): boolean {
-        return (fruit == TropicalFruit.Banana);
+    //% weight=81
+    //% blockId=tropic_pick block="pick a banana"
+    export function pickBanana(): banana {
+        return new banana();
+    }
+
+    /**
+     * Compost a fruit by letting it rot.
+     * @param matter plant matter to compost
+     */
+    //% weight=80
+    //% blockId=tropic_compost block="compost %matter"
+    export function compost(matter: banana): void {
+        matter.rot();
     }
 }
